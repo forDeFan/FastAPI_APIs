a510c84c603b8aada066a1825fb7899d80139910
tom <t-gutowski@o2.pl>
2022-10-08 17:48:53 +0200

    feat(router): add user router

---
+++ app/api/routers/route_user.py
@@ -0,0 +1,57 @@
+from app.core.user_repo import User_Repo
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="app/web/templates/")
+user_router = APIRouter()
+
+
+@user_router.get("/user")
+async def get_user_operations(request: Request) -> Jinja2Templates:
+    return templates.TemplateResponse(name="user/user_operations.html", context={"request": request})
+
+
+@user_router.get("/user/all")
+async def get_all_users(request: Request) -> Jinja2Templates:
+    users = await User_Repo.get_all()
+    return templates.TemplateResponse(name="user/user_all.html", context={"request": request, "users": users})
+
+
+@user_router.get("/user/{username}")
+async def get_user(request: Request, username: str) -> Jinja2Templates:
+    user = await User_Repo.get_by_username(username=username)
+    if user is None:
+        return templates.TemplateResponse(name="error/error_general.html", context={"request": request})
+    else:
+        return templates.TemplateResponse(name="user/user_info.html", context={"request": request, "user": user})
+
+
+@user_router.patch("/user/")
+async def update_user_password(request: Request, username: str, password: str) -> Jinja2Templates:
+    user = await User_Repo.update_user_password(username=username, password=password)
+    if user is None:
+        return templates.TemplateResponse(name="error/error_general.html", context={"request": request})
+    else:
+        updated_user = await User_Repo.get_user(username=username)
+        return templates.TemplateResponse(name="user/user_info.html", context={"request": request, "user": updated_user})
+
+
+@user_router.post("/user/")
+async def add_user(request: Request, username: str, email: str, password: str) -> Jinja2Templates:
+    user = await User_Repo.add_user(username=username, email=email, password=password)
+    try:
+        user.id
+        users = await User_Repo.get_all()
+        return templates.TemplateResponse(name="user/user_all.html", context={"request": request, "users": users})
+    except AttributeError:
+        return templates.TemplateResponse(name="error/error_general.html", context={"request": request, "error": user})
+
+
+@user_router.delete("/user/")
+async def delete_user(request: Request, username: str) -> Jinja2Templates:
+    user = await User_Repo.delete_user(username=username)
+    users = await User_Repo.get_all()
+    if user:
+        return templates.TemplateResponse(name="user/user_all.html", context={"request": request, "users": users})
+    else:
+        return templates.TemplateResponse(name="error/error_general.html", context={"request": request})
