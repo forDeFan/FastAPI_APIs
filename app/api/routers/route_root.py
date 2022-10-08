ba241fb17678731ccc6ad4e93e84c6628d7bfa8e
tom <t-gutowski@o2.pl>
2022-10-08 20:28:58 +0200

    feat(response): add response type

--- app/api/routers/route_root.py
+++ app/api/routers/route_root.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="app/web/templates/")
 root_router = APIRouter()
 
 
-@root_router.get("/")
+@root_router.get("/", response_class=HTMLResponse)
 async def home(request: Request) -> Jinja2Templates:
     return templates.TemplateResponse(name="home.html", context={"request": request})
