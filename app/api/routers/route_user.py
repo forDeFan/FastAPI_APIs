0d0bc9fb521a7baee72dbfed3255590e7022a209
tom <t-gutowski@o2.pl>
2022-10-11 23:27:56 +0200

    feat(forms): add user forms to endpoints

--- app/api/routers/route_user.py
+++ app/api/routers/route_user.py
@@ -1,8 +1,9 @@
+from app.api.forms.add_user_form import Add_User_Form
+from app.api.forms.check_user_form import Check_User_Form
 from app.core.user_repo import User_Repo
-from fastapi import APIRouter, Request, HTTPException
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from app.api.forms.check_user_form import Check_User_Form
 
 templates = Jinja2Templates(directory="app/web/templates/")
 user_router = APIRouter()
@@ -24,21 +25,14 @@ async def get_user(request: Request, username: str) -> Jinja2Templates:
     form = Check_User_Form(request=request)
     await form.load_data()
     if await form.is_valid():
-        try:
-            user = await User_Repo.get_by_username(username=username)
-            if user is not None:
-                response = templates.TemplateResponse(
-                    name="user/user_info.html", context={"request": request, "user": user})
-                return response
-            else:
-                return templates.TemplateResponse(name="error/error_general.html", context={"request": request})
-        except HTTPException:
-            form.__dict__.update(msg="")
-            form.__dict__.get("errors").append(
-                "username")
-            return templates.TemplateResponse("user/user_operations.html", form.__dict__)
-    else:
-        return templates.TemplateResponse(name="error/error_general.html", context={"request": request})
+        user = await User_Repo.get_by_username(username=username)
+        if user is not None:
+            response = templates.TemplateResponse(
+                name="user/user_info.html", context={"request": request, "user": user})
+            return response
+        else:
+            return templates.TemplateResponse(name="error/error_general.html", context={"request": request})
+    return templates.TemplateResponse(name="error/error_general.html", context={"request": request})
 
 
 @user_router.patch("/user/", response_class=HTMLResponse)
@@ -52,14 +46,20 @@ async def update_user_password(request: Request, username: str, password: str) -
 
 
 @user_router.post("/user/", response_class=HTMLResponse)
-async def add_user(request: Request, username: str, email: str, password: str) -> Jinja2Templates:
-    user = await User_Repo.add_user(username=username, email=email, password=password)
-    try:
-        user.id
-        users = await User_Repo.get_all()
-        return templates.TemplateResponse(name="user/user_all.html", context={"request": request, "users": users})
-    except AttributeError:
-        return templates.TemplateResponse(name="error/error_general.html", context={"request": request, "error": user})
+async def add_user(request: Request) -> Jinja2Templates:
+    form = Add_User_Form(request=request)
+    await form.load_data()
+    if await form.is_valid():
+        user = await User_Repo.add_user(username=form.username, email=form.email, password=form.password)
+        if user is not None:
+            user = await User_Repo.get_by_username(username=user.username)
+            response = templates.TemplateResponse(
+                name="user/user_info.html", context={"request": request, "user": user})
+            return response
+        else:
+            form.errors.append(
+                "User already exists with username or email")
+    return templates.TemplateResponse("user/user_operations.html", form.__dict__)
 
 
 @user_router.delete("/user/", response_class=HTMLResponse)
