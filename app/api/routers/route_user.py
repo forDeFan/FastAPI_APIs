7ae73d8af6819d3ebf73c23235655653ca6aef24
tom <t-gutowski@o2.pl>
2022-10-18 20:23:32 +0200

    fix(get user): fix to get single user or None

--- app/api/routers/route_user.py
+++ app/api/routers/route_user.py
@@ -14,26 +14,28 @@ async def get_user_operations(request: Request) -> Jinja2Templates:
     return templates.TemplateResponse(name="user/user_operations.html", context={"request": request})
 
 
-@user_router.get("/user/all", response_class=HTMLResponse)
+@user_router.get("/user/get/all", response_class=HTMLResponse)
 async def get_all_users(request: Request) -> Jinja2Templates:
     users = await User_Repo.get_all()
     return templates.TemplateResponse(name="user/user_all.html", context={"request": request, "users": users})
 
 
-@user_router.get("/user/", response_class=HTMLResponse)
-async def get_user(request: Request, username: str) -> Jinja2Templates:
+@user_router.post("/user/get", response_class=HTMLResponse)
+async def get_user(request: Request) -> Jinja2Templates:
     form = Check_User_Form(request=request)
     await form.load_data()
     if await form.is_valid():
-        user = await User_Repo.get_by_username(username=username)
+        user = await User_Repo.get_by_username(username=form.username)
         if user is not None:
             response = templates.TemplateResponse(
                 name="user/user_info.html", context={"request": request, "user": user})
             return response
-    return templates.TemplateResponse(name="error/error_general.html", context={"request": request})
+        else:
+            form.errors.append("No such user !")
+    return templates.TemplateResponse("user/user_operations.html", form.__dict__)
 
 
-@user_router.patch("/user/", response_class=HTMLResponse)
+@user_router.patch("/user/update", response_class=HTMLResponse)
 async def update_user_password(request: Request, username: str, password: str) -> Jinja2Templates:
     user = await User_Repo.update_user_password(username=username, password=password)
     if user is None:
@@ -43,28 +45,31 @@ async def update_user_password(request: Request, username: str, password: str) -
         return templates.TemplateResponse(name="user/user_info.html", context={"request": request, "user": updated_user})
 
 
-@user_router.post("/user/", response_class=HTMLResponse)
+@user_router.post("/user/add", response_class=HTMLResponse)
 async def add_user(request: Request) -> Jinja2Templates:
     form = Add_User_Form(request=request)
     await form.load_data()
     if await form.is_valid():
         user = await User_Repo.add_user(username=form.username, email=form.email, password=form.password)
         if user is not None:
-            user = await User_Repo.get_by_username(username=user.username)
-            response = templates.TemplateResponse(
-                name="user/user_info.html", context={"request": request, "user": user})
-            return response
+            form.__dict__.update(
+                msg=f"User with username: {form.username}, added succesfully.")
         else:
             form.errors.append(
-                "User already exists with username or email")
+                "User already exists with this username or email")
     return templates.TemplateResponse("user/user_operations.html", form.__dict__)
 
 
-@user_router.delete("/user/", response_class=HTMLResponse)
-async def delete_user(request: Request, username: str) -> Jinja2Templates:
-    user = await User_Repo.delete_user(username=username)
-    users = await User_Repo.get_all()
-    if user:
-        return templates.TemplateResponse(name="user/user_all.html", context={"request": request, "users": users})
-    else:
-        return templates.TemplateResponse(name="error/error_general.html", context={"request": request})
+@user_router.post("/user/delete", response_class=HTMLResponse)
+async def delete_user(request: Request) -> Jinja2Templates:
+    form = Check_User_Form(request=request)
+    await form.load_data()
+    if await form.is_valid():
+        user = await User_Repo.delete_user(username=form.username)
+        if user == True:
+            form.__dict__.update(
+                msg=f"User with username: {form.username}, deleted succesfully.")
+        else:
+            form.errors.append(
+                "No such user.")
+    return templates.TemplateResponse("user/user_operations.html", form.__dict__)
