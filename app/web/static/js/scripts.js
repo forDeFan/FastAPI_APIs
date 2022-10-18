f50c84c580f878b8e3272b62de2124c6883fb3ce
tom <t-gutowski@o2.pl>
2022-10-18 20:19:00 +0200

    feat(js): add child removal from children in html

--- app/web/static/js/scripts.js
+++ app/web/static/js/scripts.js
@@ -1,3 +1,10 @@
 function go_back() {
     window.history.back();
 };
+
+function clear_elem(elem_id) {
+    var div = document.getElementById(elem_id);
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+};
