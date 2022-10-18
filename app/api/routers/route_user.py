a8f0af83fc88e0cd4a57d19019dd7bc767a33bd0
tom <t-gutowski@o2.pl>
2022-10-18 20:35:23 +0200

    change(route): changed route for user adding

--- app/api/routers/route_user.py
+++ app/api/routers/route_user.py
@@ -45,20 +45,18 @@ async def update_user_password(request: Request, username: str, password: str) -
         return templates.TemplateResponse(name="user/user_info.html", context={"request": request, "user": updated_user})
 
 
-@user_router.post("/user/", response_class=HTMLResponse)
+@user_router.post("/user/add", response_class=HTMLResponse)
 async def add_user(request: Request) -> Jinja2Templates:
     form = Add_User_Form(request=request)
     await form.load_data()
     if await form.is_valid():
         user = await User_Repo.add_user(username=form.username, email=form.email, password=form.password)
         if user is not None:
-            user = await User_Repo.get_by_username(username=user.username)
-            response = templates.TemplateResponse(
-                name="user/user_info.html", context={"request": request, "user": user})
-            return response
+            form.__dict__.update(
+                msg=f"User with username: {form.username}, added succesfully.")
         else:
             form.errors.append(
-                "User already exists with username or email")
+                "User already exists with this username or email")
     return templates.TemplateResponse("user/user_operations.html", form.__dict__)
 
 
