9ab2ea635b154489a0f18d7d936ca651fb92bcde
tom <t-gutowski@o2.pl>
2022-10-19 20:22:22 +0200

    feat(update form): add html form to update user password

---
+++ app/api/forms/update_user_pass_form.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from app.api.forms.check_user_form import Check_User_Form
+from fastapi import Request
+
+
+class Update_User_Pass_Form(Check_User_Form):
+
+    def __init__(self, request: Request) -> None:
+        super().__init__(request=request)
+        self.old_password: Optional[str] = None
+        self.new_password: Optional[str] = None
+
+    async def load_data(self) -> None:
+        form = await super().load_data()
+        self.old_password = form['old_password']
+        self.new_password = form['new_password']
+
+    async def is_valid(self) -> bool:
+        if not self.username or self.username.startswith(" "):
+            self.errors.append("Username is obligatory")
+        if not self.old_password or not len(self.old_password) >= 5:
+            self.errors.append("A valid old password is required")
+        if not self.new_password or not len(self.new_password) >= 5:
+            self.errors.append("A valid new password is required")
+        if not self.errors:
+            return True
+        return False
