8ee7bc3cf2f57029a92bffcc3d915095481a9e9a
tom <t-gutowski@o2.pl>
2022-10-22 13:22:44 +0200

    feat(form): add login form

---
+++ app/api/forms/login_form.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from app.api.forms.check_user_form import Check_User_Form
+from fastapi import Request
+
+
+class Login_Form(Check_User_Form):
+    def __init__(self, request: Request):
+        super().__init__(request=request)
+        self.password: Optional[str] = None
+
+    async def load_data(self):
+        form = await super().load_data()
+        self.username = form['username']
+        self.password = form['password']
+
+    async def is_valid(self):
+        if not self.username:
+            self.errors.append("Username is required")
+        if not self.password or not len(self.password) >= 5:
+            self.errors.append("A valid password is required")
+        if not self.errors:
+            return True
+        return False
