54be2701e8c4e878c9d6325e0658e86890b4e3df
tom <t-gutowski@o2.pl>
2022-10-30 10:53:21 +0100

    feat(security): add basic authentication to the routes

--- app/api/routers/route_login.py
+++ app/api/routers/route_login.py
@@ -1,6 +1,12 @@
-from app.api.forms.login_form import Login_Form
-from app.core.user_repo import User_Repo
-from fastapi import APIRouter, Request
+from typing import Dict, Union
+
+from app.api.forms.login_form import LoginForm
+from app.core.config import settings
+from app.core.security.jwt_handler import create_access_token
+from app.core.user_repo import UserRepo
+from fastapi import APIRouter, Depends, Request, Response, status
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="app/web/templates/")
@@ -8,21 +14,44 @@ login_router = APIRouter()
 
 
 @login_router.get("/login")
-def get_login(request: Request):
+def get_login(request: Request) -> Jinja2Templates:
     return templates.TemplateResponse("login.html", {"request": request})
 
 
 @login_router.post("/login")
-async def login(request: Request):
-    form = Login_Form(request)
+async def login(request: Request) -> Union[RedirectResponse, Jinja2Templates]:
+    form = LoginForm(request)
     await form.load_data()
     if await form.is_valid():
-        user = await User_Repo.get_by_username(username=form.username)
-        if user is not None:
-            if user.password == form.password:
-                form.__dict__.update(msg="Login Successful :)")
+        db_user = await UserRepo.get_by_username(username=form.username)
+        if db_user is not None:
+            if db_user.password == form.password:
+                response = RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
+                # Produce cookie with JWT calling token endpoint.
+                await get_access_token(response=response, form_data=form)
+                return response
             else:
                 form.errors.append("You provided wrong data.")
         else:
             form.errors.append("Incorrect email or password :(")
     return templates.TemplateResponse("login.html", form.__dict__)
+
+
+@login_router.post("/token")
+async def get_access_token(
+    response: Response, form_data: OAuth2PasswordRequestForm = Depends()
+) -> Dict[str, str]:
+    user = await UserRepo.get_by_username(username=form_data.username)
+    access_token = create_access_token(data={"username": user.username})
+    # HttpOnly - prevent JS from reading the cookie.
+    response.set_cookie(
+        key=settings.COOKIE_NAME, value=f"Bearer {access_token}", httponly=True
+    )
+    return {settings.COOKIE_NAME: access_token, "token_type": "bearer"}
+
+
+@login_router.get("/logout", response_class=HTMLResponse)
+def logout() -> RedirectResponse:
+    response = RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
+    response.delete_cookie(settings.COOKIE_NAME)
+    return response
--- app/api/routers/route_user.py
+++ app/api/routers/route_user.py
@@ -1,13 +1,21 @@
 from app.api.forms.add_user_form import AddUserForm
 from app.api.forms.check_user_form import CheckUserForm
 from app.api.forms.update_user_pass_form import UpdateUserPassForm
+from app.core.config import settings
+from app.core.security.jwt_handler import oauth2_scheme
 from app.core.user_repo import UserRepo
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="app/web/templates/")
 user_router = APIRouter()
+
+
+def is_logged_in(request: Request) -> bool:
+    if not settings.COOKIE_NAME in request.cookies:
+        return False
+    return True
 
 
 @user_router.get("/user", response_class=HTMLResponse)
@@ -46,49 +54,62 @@ async def update_user_password(request: Request) -> Jinja2Templates:
     form = UpdateUserPassForm(request=request)
     await form.load_data()
     if await form.is_valid():
-        user = await UserRepo.get_by_username(username=form.username)
-        if user is not None:
-            if user.password == form.old_password:
-                await UserRepo.update_user_password(
-                    username=form.username, password=form.new_password
-                )
-                form.__dict__.update(
-                    msg=f"Password for user: {form.username}, changed successfully."
-                )
-            else:
-                form.errors.append("Wrong old password provided!")
+        if not is_logged_in(request=request):
+            form.errors.append("Update not allowed without log in!")
         else:
-            form.errors.append("No such user!")
+            user = await UserRepo.get_by_username(username=form.username)
+            if user is not None:
+                if user.password == form.old_password:
+                    await UserRepo.update_user_password(
+                        username=form.username, password=form.new_password
+                    )
+                    form.__dict__.update(
+                        msg=f"Password for user: {form.username}, changed successfully."
+                    )
+                else:
+                    form.errors.append("Wrong old password provided!")
+            else:
+                form.errors.append("No such user!")
     return templates.TemplateResponse("user/user_operations.html", form.__dict__)
 
 
-@user_router.post("/user/add", response_class=HTMLResponse)
+@user_router.post(
+    "/user/add", dependencies=[Depends(oauth2_scheme)], response_class=HTMLResponse
+)
 async def add_user(request: Request) -> Jinja2Templates:
     form = AddUserForm(request=request)
     await form.load_data()
     if await form.is_valid():
-        user = await UserRepo.add_user(
-            username=form.username, email=form.email, password=form.password
-        )
-        if user is not None:
-            form.__dict__.update(
-                msg=f"User with username: {form.username}, added succesfully."
-            )
+        if not is_logged_in(request=request):
+            form.errors.append("Adding not allowed without log in!")
         else:
-            form.errors.append("User already exists with this username or email.")
+            user = await UserRepo.add_user(
+                username=form.username, email=form.email, password=form.password
+            )
+            if user is not None:
+                form.__dict__.update(
+                    msg=f"User with username: {form.username}, added succesfully."
+                )
     return templates.TemplateResponse("user/user_operations.html", form.__dict__)
 
 
-@user_router.post("/user/delete", response_class=HTMLResponse)
+@user_router.post(
+    "/user/delete", dependencies=[Depends(oauth2_scheme)], response_class=HTMLResponse
+)
 async def delete_user(request: Request) -> Jinja2Templates:
     form = CheckUserForm(request=request)
     await form.load_data()
     if await form.is_valid():
-        user = await UserRepo.delete_user(username=form.username)
-        if user == True:
-            form.__dict__.update(
-                msg=f"User with username: {form.username}, deleted succesfully."
-            )
+        if not is_logged_in(request=request):
+            form.errors.append("Delete not allowed without log in!")
         else:
-            form.errors.append("No such user!")
+            user = await UserRepo.delete_user(username=form.username)
+            if user is True:
+                form.__dict__.update(
+                    msg=f"User with username: {form.username}, deleted succesfully."
+                )
+            if user is False:
+                form.errors.append("Admin can not be removed !")
+            else:
+                form.errors.append("No such user!")
     return templates.TemplateResponse("user/user_operations.html", form.__dict__)
