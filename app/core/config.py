724171edaf3fd1d646749206f7a4fcfc8f2cba25
tom <t-gutowski@o2.pl>
2022-10-30 10:51:23 +0100

    feat(securoty): add jwt and admin variables

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,11 @@ load_dotenv()
 class Settings(BaseSettings):
     DB_URL: str = os.getenv('DATABASE_URL')
     DB_SECRET: str = os.getenv('DB_SECRET')
+    JWT_SECRET_KEY: str = os.getenv('JWT_SECRET_KEY')
+    JWT_EXPIRE_MINUTES: int = 15
+    JWT_ALGORITHM: str = "HS256"
+    COOKIE_NAME: str = "access_token"
+    ADMIN_PASSWORD: str = os.getenv('ADMIN_PASSWORD')
 
 
 settings = Settings()
