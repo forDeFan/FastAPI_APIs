34e15b8b5bd3a9bf06f39c61046c00e605a3159f
tom <t-gutowski@o2.pl>
2022-10-31 21:01:16 +0100

    feat(log out): add log out in template

--- app/api/routers/route_login.py
+++ app/api/routers/route_login.py
@@ -1,3 +1,4 @@
+from os import link
 from typing import Dict, Union
 
 from app.api.forms.login_form import LoginForm
@@ -26,10 +27,7 @@ async def login(request: Request) -> Union[RedirectResponse, Jinja2Templates]:
         db_user = await UserRepo.get_by_username(username=form.username)
         if db_user is not None:
             if db_user.password == form.password:
-                form.__dict__.update(
-                    msg=f"{db_user.username.capitalize()}"
-                )
-                response = templates.TemplateResponse("login.html", form.__dict__)
+                response = RedirectResponse(url="/user", status_code=status.HTTP_302_FOUND)
                 # Produce cookie with JWT calling token endpoint.
                 await get_access_token(response=response, form_data=form)
                 return response
@@ -55,6 +53,6 @@ async def get_access_token(
 
 @login_router.get("/logout", response_class=HTMLResponse)
 def delete_cookie(response: Response) -> RedirectResponse:
-    response = RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
+    response = RedirectResponse(url="/login", status_code=status.HTTP_302_FOUND)
     response.delete_cookie(settings.COOKIE_NAME)
     return response
