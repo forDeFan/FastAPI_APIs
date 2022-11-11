4a7c266de8e457beabf66b5725ef0197d1eaf83c
tom <t-gutowski@o2.pl>
2022-11-11 13:52:10 +0100

    fix(token validation): change token expire time validation - to be used on endpoints. Remove previous docstrings form changed methods.

--- app/core/security/jwt_handler.py
+++ app/core/security/jwt_handler.py
@@ -5,7 +5,7 @@ from app.core.config import settings
 from app.core.db.user_model import User
 from app.core.user_repo import UserRepo
 from fastapi import Request
-from jose import JWTError, jwt
+from jose import jwt, JWTError
 
 
 def create_access_token(data: Dict) -> str:
@@ -19,52 +19,41 @@ def create_access_token(data: Dict) -> str:
         str: encoded token
     """
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(settings.JWT_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(
+        minutes=settings.JWT_EXPIRE_MINUTES
+    )
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
-        to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM
+        to_encode,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM,
     )
     return encoded_jwt
 
 
-async def decode_access_token(token: str) -> Union[User, None]:
-    """
-    Decode JWT token.
-
-    Args:
-        token (str): to be decoded
-
-    Returns:
-        Union[User, None]:
-            User object if user exsist and decoding process was succesfull
-            None if user do not exists or JWT decoding error
-    """
+async def decode_access_token(token: str):
     token = token.lstrip("Bearer").strip()
     try:
         payload = jwt.decode(
-            token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM]
+            token,
+            settings.JWT_SECRET_KEY,
+            algorithms=[settings.JWT_ALGORITHM],
         )
-        username: str = payload.get("username")
     except JWTError:
-        return None
+        payload = None
 
-    user = await UserRepo.get_by_username(username=username)
-    return user
+    return payload
 
 
-async def get_current_user_from_cookie(request: Request) -> Union[User, None]:
-    """
-    Extract authorization cookie from request, and decode token
-    to get user from database.
-
-    Args:
-        request (Request): from which auyhorization cookie will be extracted
-
-    Returns:
-        Union[User, None]:
-            User object taken from database if user exists
-            None if such user do not exists in database or token decoding error
-    """
+async def get_current_user_from_cookie(
+    request: Request,
+) -> Union[User, None]:
     token = request.cookies.get(settings.COOKIE_NAME)
-    user = await decode_access_token(token)
+    try:
+        payload = await decode_access_token(token=token)
+        username = payload.get("username")
+        user = await UserRepo.get_by_username(username=username)
+    except:
+        user = None
+
     return user
